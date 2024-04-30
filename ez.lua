b925e17c7987810677fd8a966570399c7a50bc27
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-04-30 20:29:48 +0700

    Update ez.lua

--- ez.lua
+++ ez.lua
@@ -4,6 +4,6 @@ local Pitch = 1
   
    for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
 			if v:IsA("RemoteEvent") and v.Name == "AC6_FE_Sounds" then
-		           v:FireServer("newSound", Name, workspace, ID, Pitch, Volume, true)
+		           v:FireServer("newSound", Name, workspace, (txt), Pitch, Volume, true)
 			      v:FireServer("playSound", Name)
 			end 
