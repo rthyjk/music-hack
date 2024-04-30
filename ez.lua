f643f4f09b974624269d21111d0a78d292b443bb
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-04-30 20:25:07 +0700

    Add files via upload

---
+++ ez.lua
@@ -0,0 +1,9 @@
+local Name = "get troll" 
+local Volume = 100 
+local Pitch = 1 
+  
+   for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+			if v:IsA("RemoteEvent") and v.Name == "AC6_FE_Sounds" then
+		           v:FireServer("newSound", Name, workspace, ID, Pitch, Volume, true)
+			      v:FireServer("playSound", Name)
+			end 
