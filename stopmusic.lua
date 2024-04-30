2e952279f406b8bc8c2ba5dd93f3858fc82e74c7
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-04-30 21:00:05 +0700

    Create stopmusic.lua

---
+++ stopmusic.lua
@@ -0,0 +1,18 @@
+sound = false
+local ID = "rbxassetid://9062298855" 
+local Name = "get troll" 
+local Volume = 100 
+local Pitch = 1 
+
+           for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
+			if v:IsA("RemoteEvent") and v.Name == "AC6_FE_Sounds" then
+				if sound == true then
+					v:FireServer("newSound", Name, workspace, ID, Pitch, Volume, true)
+					v:FireServer("playSound", Name)
+				end 
+
+				if sound == false then
+					v:FireServer("stopSound", Name)
+				end
+			end
+		end
