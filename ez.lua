62b7a8f3e56f4afb23c85293a23e56f16c1a88ec
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-04-30 20:46:25 +0700

    Update and rename ez.lua to music.lua

--- ez.lua
+++
@@ -1,9 +0,0 @@
-local Name = "get troll" 
-local Volume = 100 
-local Pitch = 1 
-  
-   for i,v in pairs(game:GetService("Workspace"):GetDescendants()) do
-			if v:IsA("RemoteEvent") and v.Name == "AC6_FE_Sounds" then
-		           v:FireServer("newSound", Name, workspace, (txt), Pitch, Volume, true)
-			      v:FireServer("playSound", Name)
-			end 
