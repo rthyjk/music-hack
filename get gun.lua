d8c2a374c74b6a9ea539f36c95be87488de4545f
rthyjk <96871930+rthyjk@users.noreply.github.com>
2024-05-04 19:03:53 +0700

    Create get gun.lua

---
+++ get gun.lua
@@ -0,0 +1,18 @@
+--[[Polica or RTA 
+    by Bkkpro1980]]--
+if game.Teams:FindFirstChild("Head Developer") then
+    for i, tool in pairs(game.Teams["Head Developer"]:GetChildren()) do
+        ClonedTool =  tool:Clone()
+        ClonedTool.Parent = game.Players.LocalPlayer.Backpack
+    end
+elseif game.Teams:FindFirstChild("A26 | หน่วยอรินทราช") and game.Teams:FindFirstChild("BPP | ตำรวจตระเวนชายแดน") then
+    for i, tool in pairs(game.Teams["A26 | หน่วยอรินทราช"]:GetChildren()) do
+        ClonedTool =  tool:Clone()
+        ClonedTool.Parent = game.Players.LocalPlayer.Backpack
+    end
+    game.Teams["BPP | ตำรวจตระเวนชายแดน"].M16A4:Clone().Parent = game.Players.LocalPlayer.Backpack
+else
+    warn("Not in compatible game!")
+end
+[14:21]
+bye
